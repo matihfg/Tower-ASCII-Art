9f8e211e7004a518de6eae6c276ba7a4f5c4239a
matihfg <matihfg@gmail.com>
2017-02-10 01:46:08 -0600

    Update Tower.java

--- Tower.java
+++ Tower.java
@@ -1,23 +1,7 @@
-/*
- *  CS312 Assignment 2.
- *  On my honor, <NAME>, this programming assignment is my own work.
- *
- *  A program to print out the UT Tower in ASCII art form.
- *
- *  Name: Matias Fragoso
- *  email address: mfragoso@utexas.edu
- *  UTEID: mhf465
- *  Section 5 digit ID: 51290
- *  Grader name:
- *  Number of slip days used on this assignment:
- */
-
 public class Tower {
     
-    // CS312 students, DO NOT ALTER THE FOLLOWING LINE except for the literal int.
     // You may change the literal int assigned to SIZE to any value from 2 to 100.
-    // In the final version of the program you submit set the SIZE to 3.
-    public static final int SIZE = 4;
+    public static final int SIZE = 3;
 
     public static void main(String[] args) {
         drawBell();
